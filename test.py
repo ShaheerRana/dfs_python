a87b50594dffdc6696f813197db1cf592e4c5694
ShaheerRana <s32rana@uwaterloo.ca>
2022-02-07 17:06:00 -0500

    more UART scripting

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
-import serial
+'''import serial
 print("Conversion Open")
 while (1):
     string = input();
 
 
     string = string.replace(",", " +")
-    print('=', string[1:-1])
+    print('=', string[1:-1])'''
+f = open('dfs_uart.txt','wb') 
+while(1):
+    f.write(b"0x100")
+f.close()
