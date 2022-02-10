08b8ef8da8342b91392519ce21634a9d12e79914
ShaheerRana <s32rana@uwaterloo.ca>
2022-02-10 16:58:46 -0500

    uart

--- import_serial.py
+++ import_serial.py
@@ -2,9 +2,9 @@ import serial
 import binascii
 print ("Active");
 serialPort = serial.Serial(port = "COM5", baudrate=115200, bytesize=8, timeout=0, stopbits=serial.STOPBITS_ONE)    
-serialString = 0;                          # Used to hold data coming over UART
+serialString = b'\x00';                          # Used to hold data coming over UART
 f = open('dfs_uart.txt','wb') 
-f.write(b"0x100")
+#f.write(b"0x100")
 
 while(1):
 
@@ -13,18 +13,17 @@ while(1):
 
         # Read data out of the buffer until a carraige return / new line is found
         #serialString = serialPort.readline()
-        serialString = serialPort.read(1).replace("b'\\r\\r\\n","")
+        serialString = serialPort.read(1)
         #serialString = serialPort.read(128).replace("b'\\r\\r\\n","")
         # Print the contents of the serial data
         #print(serialString.decode('Ascii'))
-        print(binascii.unhexlify('0%x' % serialString))
+        #print(binascii.unhexlify('0%x' % serialString))
         #print(int(binascii.hexlify(serialString),16))
-        #print (serialString)
+        print (int.from_bytes(serialString, byteorder='big'))
 
-        #f.write(serialString)
+        f.write(serialString)
 
         # Tell the device connected over the serial port that we recevied the data!
         # The b at the beginning is used to indicate bytes!
-        serialPort.write(b"Thank you for sending data \r\n")
+        #serialPort.write(b"Thank you for sending data \r\n")
 f.close()
-    
--- test.py
+++ test.py
@@ -7,6 +7,12 @@ while (1):
     string = string.replace(",", " +")
     print('=', string[1:-1])'''
 f = open('dfs_uart.txt','wb') 
-while(1):
-    f.write(b"0x100")
+a = 1
+#while(1):
+#f.write(a.to_bytes(1, byteorder='big'))
+x = 1
+while (x < 100):
+    f.write((a).to_bytes(1, byteorder='big', signed=True))
+    x+=1
 f.close()
+print ("done")
