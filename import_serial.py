aaf7ccac5e95573fc4e49c8f123c6ea3353bf3db
brandondeath <brandon.death@outlook.com>
2022-03-23 11:01:30 -0400

    fixed double reading of bytes

--- import_serial.py
+++ import_serial.py
@@ -1,7 +1,7 @@
 import serial
 import binascii
 print ("Active");
-serialPort = serial.Serial(port = "COM5", baudrate=115200, bytesize=8, timeout=0, stopbits=serial.STOPBITS_ONE)    
+serialPort = serial.Serial(port = "COM7", baudrate=115200, bytesize=8, timeout=0, stopbits=serial.STOPBITS_ONE)    
 serialString = b'\x00';                          # Used to hold data coming over UART
 f = open('dfs_uart.txt','wb') 
 #f.write(b"0x100")
@@ -9,17 +9,17 @@ f = open('dfs_uart.txt','wb')
 while(1):
 
     # Wait until there is data waiting in the serial buffer
-    if(serialPort.in_waiting > 0):
+    if(serialPort.in_waiting > 1):
 
         # Read data out of the buffer until a carraige return / new line is found
         #serialString = serialPort.readline()
-        serialString = serialPort.read(1)
+        serialString = serialPort.read(2)
         #serialString = serialPort.read(128).replace("b'\\r\\r\\n","")
         # Print the contents of the serial data
         #print(serialString.decode('Ascii'))
         #print(binascii.unhexlify('0%x' % serialString))A
         #print(int(binascii.hexlify(serialString),16))
-        print (int.from_bytes(serialString, byteorder='big'))
+        print (int.from_bytes(serialString, byteorder='little'))
 
         f.write(serialString)
 
