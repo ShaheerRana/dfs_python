f071a264d7749d5050d75fcf4fc78a4e02285381
Shaheer Rana <s32rana@uwaterloo.ca>
2022-04-07 05:15:29 -0400

    pyqt implementation

---
+++ faster_adc_plotting.py
@@ -0,0 +1,51 @@
+from PyQt5 import QtWidgets, QtCore
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg
+import sys  # We need sys so that we can pass argv to QApplication
+import os
+import csv
+  
+Names = []
+Values = []
+  
+with open('dfs_uart.csv','r') as csvfile:
+    lines = csv.reader(csvfile, delimiter=',')
+    for row in lines:
+        Names.append(row[0])
+        Values.append(float(row[1]))
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.graphWidget = pg.PlotWidget()
+        self.setCentralWidget(self.graphWidget)
+
+        #Add Background colour to white
+        self.graphWidget.setBackground('w')
+        # Add Title
+        self.graphWidget.setTitle("Your Title Here", color="b", size="30pt")
+        # Add Axis Labels
+        styles = {"color": "#f00", "font-size": "20px"}
+        self.graphWidget.setLabel("left", "ADC Value", **styles)
+        self.graphWidget.setLabel("bottom", "Sample", **styles)
+        #Add legend
+        self.graphWidget.addLegend()
+        #Add grid
+        self.graphWidget.showGrid(x=True, y=True)
+        #Set Range
+        #self.graphWidget.setXRange(0, 10, padding=0)
+        #self.graphWidget.setYRange(20, 55, padding=0)
+
+        pen = pg.mkPen(color=(255, 0, 0))
+        self.graphWidget.plot(Names, Values, name="Sensor 1",  pen=pen)
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
